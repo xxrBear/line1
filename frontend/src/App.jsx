9d738a4d11aa742daca70b8e9b1a50e9210c3160
bear <xxrBear@gmail.com>
2025-11-15 17:16:09 +0800

    前端初始化

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+import { Button } from "@mui/material";
+
+function App() {
+  return (
+    <div style={{ padding: 50 }}>
+      <Button variant="contained" color="primary">
+        Hello MUI
+      </Button>
+    </div>
+  );
+}
+
+export default App;
