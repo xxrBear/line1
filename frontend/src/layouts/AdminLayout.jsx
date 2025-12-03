5695cb4abaea9960e385fef5f24ddd79584a0ef0
熊大如如 <xxrBear@gmail.com>
2025-12-03 17:54:48 +0800

    update

--- frontend/src/layouts/AdminLayout.jsx
+++ frontend/src/layouts/AdminLayout.jsx
@@ -3,13 +3,6 @@ import { Outlet, Link } from "react-router-dom";
 const AdminLayout = () => {
     return (
         <>
-            <aside>
-                <ul >
-                    <li><Link to="/" >仪表盘</Link></li>
-                    <li><Link to="/articles">文章管理</Link></li>
-                </ul>
-            </aside>
-
             <main>
                 <Outlet />
             </main>
