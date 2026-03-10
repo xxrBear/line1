8e80464efbd49222669ded9a8a805da2eefa07b9
熊大如如 <xxrBear@gmail.com>
2026-03-10 16:34:00 +0800

    增加 blog/static

--- line1/settings/base.py
+++ line1/settings/base.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 SECRET_KEY = "django-insecure-n(sqdhyakh3ph$03a67*cmb0hwd4d)$02$4ulncuhl%-+md76t"
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -23,7 +23,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.base',
-    'apps.user',
     'apps.blog',
 ]
 
