8b3bfbe6fc720f6a05496cf6c0e2bfd2c680adf9
熊大如如 <xxrBear@gmail.com>
2026-03-18 15:34:12 +0800

    接入 PG 数据库

--- line1/settings/base.py
+++ line1/settings/base.py
@@ -45,8 +45,12 @@ WSGI_APPLICATION = "line1.wsgi.application"
 # Database
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": 'line1',
+        "HOST": '127.0.0.1',
+        'PORT': 5432,
+        'USER': 'postgres',
+        'PASSWORD': '123456',
     }
 }
 
