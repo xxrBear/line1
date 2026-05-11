44971b29e4d4fcbcf513998625e4e04065e8a081
xxrBear <xxrBear@gmail.com>
2026-05-11 17:37:58 +0800

    修改 BaseModel 模型

--- apps/base/models.py
+++ apps/base/models.py
@@ -2,7 +2,9 @@ from django.db import models
 
 
 class BaseModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+    created_at = models.DateTimeField(
+        auto_now_add=True, db_index=True, verbose_name="创建时间"
+    )
     updated_at = models.DateTimeField(auto_now=True, verbose_name="更新时间")
 
     class Meta:
