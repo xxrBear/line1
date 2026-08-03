68750f60d4c9377338257352b18a5b41191e7fca
xxrBear <xxrBear@gmail.com>
2026-08-03 10:30:06 +0800

    move: settings folder

--- settings/base.py
+++ settings/base.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 import environ
 
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 environ.Env.read_env(BASE_DIR / '.env')
 
--- settings/prod.py
+++ settings/prod.py
@@ -1,4 +1,4 @@
-from .base import *  # noqa: F403
+from settings.base import *  # noqa: F403
 
 DEBUG = False
 
